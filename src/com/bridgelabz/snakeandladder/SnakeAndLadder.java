862c88ef0d2fdedf3117e060641d480fc47e98fd
VighneshBilgi <vhbilgi@gmail.com>
2022-04-29 22:29:09 +0530

    Added: UC1_Player_At_Position_0

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
+
+    static int playPos = 0;
+
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game:");
     }
