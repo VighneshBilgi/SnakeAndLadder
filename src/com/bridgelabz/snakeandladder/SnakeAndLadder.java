c7ed27d77753543fdd3aaaaaf219f4c7683d2623
VighneshBilgi <vhbilgi@gmail.com>
2022-04-29 22:39:59 +0530

    Added: UC2_Die_Number

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -6,5 +6,8 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game:");
+
+        int die = (int)(Math.random()*10 %6) +1;
+        System.out.println("Die Number is:"+die);
     }
 }
